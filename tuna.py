5d4d355ab8d9707ee47ba2764589b61e0fffcc17
mority <mority@posteo.net>
2025-08-07 12:49:57 +0200

    wip

--- tuna.py
+++ tuna.py
@@ -1,27 +1,51 @@
 from load import load
-from util import uses_taxi
+from util import uses_taxi, mam
 
 import pandas as pd
-from datetime import datetime
+from datetime import date
 
-service_start = 4 * 60
+day_start = pd.Timedelta(hours=4)
+day_end = pd.Timedelta(minutes=1439)
 
-# time until next arrival
-def tuna(t, itineraries):
-    arr_min = datetime.max
+
+def find_date(itineraries: list) -> date | None:
+    if not itineraries:
+        return None
+    return pd.Timestamp(itineraries[0]["startTime"]).tz_convert("Europe/Berlin").date()
+
+
+def find_next(t: pd.Timestamp, itineraries: list) -> list[pd.Timestamp]:
+    next_arr = pd.Timestamp.max
+    next_dep = pd.Timestamp.max
     for i in itineraries:
-        dep = datetime.fromisoformat(i['startTime'])
-        arr = datetime.fromisoformat(i['endTime'])
-        if t <= dep and arr < arr_min:
-            arr_min = arr
-    return arr_min - t
+        dep = pd.Timestamp(i["startTime"]).tz_convert("Europe/Berlin")
+        arr = pd.Timestamp(i["endTime"]).tz_convert("Europe/Berlin")
+        if t <= dep and arr < next_arr:
+            next_dep = dep
+            next_arr = arr
+    if next_arr == pd.Timestamp.max:
+        return []
+    else:
+        return [next_dep, next_arr]
 
-def tunas(itineraries):
-    ret = [0] * 1440
-    for t in range(service_start, 1440):
-        ret[t] = tuna(t,itineraries)
+
+def tuna(itineraries: list) -> list[pd.Timedelta | None]:
+    ret: list[pd.Timedelta | None] = [None] * 1440
+    d = find_date(itineraries)
+    if d is None:
+        return ret
+    journey: list[pd.Timestamp] = []
+    for t in pd.date_range(
+        start=d + day_start, end=d + day_end, freq="min", inclusive="both"
+    ):
+        if not journey or journey[0] < t:
+            journey = find_next(t, itineraries)
+        if not journey:
+            break
+        ret[mam(t)] = journey[1] - t
     return ret
+
 
 df = load()
 tuna = [0] * 1440
-print(type(df.at[0, 'itineraries'][0]['startTime']))
+print(type(df.at[0, "itineraries"][0]["startTime"]))
--- util.py
+++ util.py
@@ -1,5 +1,12 @@
+import pandas as pd
+
+
 def uses_taxi(legs):
     for leg in legs:
-        if leg['mode'] == 'ODM':
+        if leg["mode"] == "ODM":
             return True
     return False
+
+
+def mam(t: pd.Timestamp) -> int:
+    return t.hour * 60 + t.minute
